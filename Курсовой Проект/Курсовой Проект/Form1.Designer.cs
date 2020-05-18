ff5e8771b345e003249036f1c80ff016f01205a7
kydesnikova <63052956+kydesnikova@users.noreply.github.com>
2020-05-18 21:23:58 +0300

    Add files via upload

---
+++ Курсовой Проект/Курсовой Проект/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Курсовой_Проект
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 291);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Azure;
+            this.button1.Location = new System.Drawing.Point(461, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(548, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(548, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(530, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(530, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "y";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar1.Location = new System.Drawing.Point(565, 148);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar2.Location = new System.Drawing.Point(565, 200);
+            this.trackBar2.Maximum = 80;
+            this.trackBar2.Minimum = 40;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Value = 40;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll_1);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.trackBar3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar3.Location = new System.Drawing.Point(565, 252);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(484, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Размер";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(487, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Вращение";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(484, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Скорость";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(565, 304);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(463, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Время остановки";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Сброс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(670, 363);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Курсовая работа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Курсовой Проект/Курсовой Проект/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Курсовой_Проект
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Figura();
+            textBox1.Text = Convert.ToString(pictureBox1.Width);
+            textBox2.Text = Convert.ToString(pictureBox1.Height);
+            Width = pictureBox1.Width;
+            Height = pictureBox1.Height;
+        }
+        int Width;
+        int Height;
+        double dt = 0;  //у границ рабочей области  
+        private float diametr = 40;
+        private void Figura()
+        {//центр
+            float x0 = 0 + dx;
+            float y0 = pictureBox1.Height - diametr - dy;
+          
+            {
+               Bitmap Ramka = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics graph = Graphics.FromImage(Ramka);
+                byte blz = (byte)(255 - 195 / (pictureBox1.Width - diametr) * dx);//изменение цвета относительно x
+                byte rdz = (byte)(60 + 195 / (pictureBox1.Width - diametr) * dx);
+                Color col =  Color.FromArgb(255, blz, 255, rdz);
+                Color col1 =  Color.FromArgb(255, rdz, 0, blz);
+                Pen pen;
+                if (diametr > 60) { pen = new Pen(col, 2); } else { pen = new Pen(col, 1); }
+                SolidBrush brSolid = new SolidBrush(col);
+                SolidBrush brSoft = new SolidBrush(col1);
+                graph.DrawRectangle(pen, 1, 1, pictureBox1.Width - 2, pictureBox1.Height - 2);//ramka
+                graph.FillEllipse(brSolid, x0, y0, diametr, diametr);//large circle
+                                                                     //построение звезды
+                float xz0 = x0 + diametr / 2;//центр фигруы
+                float yz0 = y0 + diametr / 2;
+                float opisR = (float)((diametr / 2) / (Math.Sqrt(5) - 1));//вписанный радиус (для нижних точек)
+                double starSide = (diametr / 2 * Math.Sqrt((5 - Math.Sqrt(5)) / 2));//длина стороны , для нижних точек
+                PointF p1 = new PointF(xz0, yz0 - diametr / 2);//первая часть звезды
+                PointF p2 = new PointF((float)(xz0 + starSide / 4), yz0 - opisR / 2);
+                PointF p3 = new PointF((float)(xz0 - starSide / 4), yz0 - opisR / 2);
+                p1 = RotatePoint(p1, 90 * yg, xz0, yz0);//смещение треугольника 
+                p2 = RotatePoint(p2, 90 * yg, xz0, yz0);
+                p3 = RotatePoint(p3, 90 * yg, xz0, yz0);
+                for (int n = 0; n < 11; n++)
+                {//остальные части относительно первой
+                    PointF pNew1 = RotatePoint(p1, 60 * n, xz0, yz0);
+                    PointF pNew2 = RotatePoint(p2, 60 * n, xz0, yz0);
+                    PointF pNew3 = RotatePoint(p3, 60 * n, xz0, yz0);
+                    PointF[] treeDots = { pNew1, pNew2, pNew3 };
+                    graph.FillPolygon(brSoft, treeDots);
+                }
+             
+                pictureBox1.Image = Ramka;
+                
+            }
+
+        }
+        private PointF RotatePoint(PointF point, double n, float x0, float y0)
+        {//функция для смещения относительно точки на угол
+            float ddX = point.X - x0;
+            float ddY = point.Y - y0;
+            point.X = (float)(ddX * Math.Cos(Math.PI * n / 180) - ddY * Math.Sin(Math.PI * n / 180)) + x0;
+            point.Y = (float)(ddX * Math.Sin(Math.PI * n / 180) + ddY * Math.Cos(Math.PI * n / 180)) + y0;
+            return point;
+        }
+
+        float dx = 1;//смещение по координатам
+        float dy = 1;
+        float smesh = 3.1f;//скорость смещение по координтам
+        float proverka = 1;//чтобы проверка на выход за предел работала
+        double yg = 0;//угловое смещение
+        double ygSpeed = 0.01f;
+        
+    
+        //для движения по параболе
+        private double FomA;
+        private double FomB;
+        private double FomC;
+        private void CalcFom()
+        {
+            double x1 = 0; double x2 = (pictureBox1.Width) / 2 - diametr / 2;
+            double y1 = (pictureBox1.Height - diametr); double y2 = 2;
+            FomA = Math.Sqrt((y1 - y2) / Math.Pow(x1 / x2 - 1, 2));
+            FomB = pictureBox1.Height - diametr - y2;
+            FomC = FomA / x2;
+        }
+        private float move(float x)
+        {
+            CalcFom();
+            return (float)(-Math.Pow(FomC * x - FomA, 2) + FomB);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                button1.Text = "Старт";
+            }
+            else
+            {
+                button1.Text = "Стоп";
+                timer1.Start();
+            }
+            if (Convert.ToInt32(textBox1.Text) < Width && Convert.ToInt32(textBox1.Text) > 1)
+            {
+                pictureBox1.Width = Convert.ToInt32(textBox1.Text);
+            }
+            else { pictureBox1.Width = Width; }
+            if (Convert.ToInt32(textBox2.Text) < Height && Convert.ToInt32(textBox1.Text) > 1)
+            {
+                pictureBox1.Height = Convert.ToInt32(textBox2.Text);
+            }
+            else { pictureBox1.Height = Height; }
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+        }
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            PicDef();
+            Figura();
+
+        }
+        private void PicDef()
+        {
+            dx = dy = 0;
+            yg = 0;
+            timer1.Stop();
+            button1.Text = "Start";
+            label2.Text = "x";
+            label3.Text = "y";
+            label3.Text = "move";
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+        }
+
+        
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Figura();
+            
+           if (dx+trackBar2.Value >= pictureBox1.Width)
+            {
+                Thread.Sleep(1000*Convert.ToInt32(textBox3.Text));
+            }
+            else
+            {
+                if (dx < 0)
+                    Thread.Sleep(1000*Convert.ToInt32(textBox3.Text));
+            }
+            if (proverka < 0)
+                {
+               
+                    dx -= smesh;
+                    dy = move(dx);
+                    proverka = -dy;
+
+                    yg -= ygSpeed;
+                }
+
+                else
+                {
+                    dx += smesh;
+                    dy = move(dx);
+                    proverka = dy;
+
+                    yg += ygSpeed;
+                }
+            label2.Text = "x: " + Convert.ToString(dx);
+            label3.Text = "y: " + Convert.ToString(dy);
+        }
+
+
+        int t = 1;
+        private int prist() { t = Convert.ToInt32(textBox1.Text) - 2; return t; }
+
+       private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (t >= 0) { timer1.Start(); timer2.Stop(); }
+            t -= 1;
+        }
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            smesh = (trackBar1.Value) / 10;
+        }
+
+        private void trackBar2_Scroll_1(object sender, EventArgs e)
+        {
+            diametr = trackBar2.Value;
+            if (!timer1.Enabled)
+            {
+                Figura();//рисует фигуру
+            }
+        }
+
+        private void trackBar3_Scroll_1(object sender, EventArgs e)
+        {
+            ygSpeed = (double)(trackBar3.Value) / 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dx = 0;
+            dy = 1;
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            Figura();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
